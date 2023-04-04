1bf4ad26cb71a9f9eb455086db0b64465f2b1f0a
hadi-darwish <hadi.darwish.03@gmail.com>
2023-04-04 10:11:32 +0300

    DG-00007 correction of Query Builder

--- src/Microservices/Database/Helpers/QueryBuilder.php
+++ src/Microservices/Database/Helpers/QueryBuilder.php
@@ -7,20 +7,23 @@
 
     class QueryBuilder {
 
-        public static function insert(
+        //TODO add buildsql function 
+        //TODO non static
+        public function insert(
             string $table,
             array $data,
             DatabaseCredentials $db
         ): string {
             $columns = array_keys($data);
             $values = array_values($data);
-            $placeholders = implode(', ', array_fill(0, count($data), '?'));
+            //TODO implode values directly
+            $placeholders = implode(', ', $data);
             $columns = implode(", ", $columns);
             $sql = "INSERT INTO $table ($columns) VALUES ($placeholders)";
             return $sql;
         }
 
-        public static function update(
+        public function update(
             string $table,
             array $data,
             array $where,
@@ -28,13 +31,13 @@
         ): string {
             $columns = array_keys($data);
             $values = array_values($data);
-            $placeholders = implode(', ', array_fill(0, count($data), '?'));
+            $placeholders = implode(', ', $data);
             $columns = implode(", ", $columns);
             $sql = "UPDATE $table SET ($columns) VALUES ($placeholders) WHERE $where";
             return $sql;
         }
 
-        public static function delete(
+        public  function delete(
             string $table,
             array $where,
             DatabaseCredentials $db
@@ -43,103 +46,151 @@
             return $sql;
         }
 
-        public static function select(
+        // public static function select(
+        //     string $table,
+        //     array $columns,
+        //     array $where,
+        //     DatabaseCredentials $db
+        // ): string {
+        //     $columns = implode(", ", $columns);
+        //     $sql = "SELECT $columns FROM $table WHERE $where";
+        //     return $sql;
+        // }
+
+
+
+        // public static function selectAll(
+        //     string $table,
+        //     DatabaseCredentials $db
+        // ): string {
+        //     $sql = "SELECT * FROM $table";
+        //     return $sql;
+        // }
+
+        // public static function selectAllWhere(
+        //     string $table,
+        //     array $where,
+        //     DatabaseCredentials $db
+        // ): string {
+        //     $sql = "SELECT * FROM $table WHERE " . implode(" AND ", $where);
+        //     return $sql;
+        // }
+
+        // public static function selectAllOrderBy(
+        //     string $table,
+        //     DatabaseCredentials $db,
+        //     string $orderBy
+        // ): string {
+        //     $sql = "SELECT * FROM $table ORDER BY $orderBy";
+        //     return $sql;
+        // }
+
+        // public static function selectAllWhereOrderBy(
+        //     string $table,
+        //     array $where,
+        //     string $orderBy
+        // ): string {
+        //     $sql = "SELECT * FROM $table WHERE $where ORDER BY $orderBy";
+        //     return $sql;
+        // }
+
+
+        // public static function selectAllJoin(
+        //     string $table,
+        //     array $columns,
+        //     array $join,
+        // ): string {
+        //     $columns = implode(", ", $columns);
+        //     $sql = "SELECT $columns FROM $table";
+        //     foreach($join as $j){
+        //         $sql .= " JOIN $j";
+        //     }
+        //     return $sql;
+        // }
+
+        // public static function selectAllJoinWhere(
+        //     string $table,
+        //     array $columns,
+        //     array $join,
+        //     array $where,
+        //     DatabaseCredentials $db
+        // ): string {
+        //     $columns = implode(", ", $columns);
+        //     $sql = "SELECT $columns FROM $table";
+        //     foreach($join as $j){
+        //         $sql .= " JOIN $j";
+        //     }
+        //     $sql .= " WHERE $where";
+        //     return $sql;
+        // }
+
+
+        // public static function selectAllJoinWhereOrderBy(
+        //     string $table,
+        //     array $columns,
+        //     array $join,
+        //     array $where,
+        //     array $orderBy,
+        //     DatabaseCredentials $db
+        // ): string {
+        //     $columns = implode(", ", $columns);
+        //     $sql = "SELECT $columns FROM $table";
+        //     foreach($join as $j){
+        //         $sql .= " JOIN $j";
+        //     }
+        //     $sql .= " WHERE $where";
+        //     $sql .= " ORDER BY $orderBy";
+        //     return $sql;
+        // }
+
+        // public static function selectAllJoinOrderBy(
+        //     string $table,
+        //     array $columns,
+        //     array $join,
+        //     array $orderBy,
+        //     DatabaseCredentials $db
+        // ): string {
+        //     $columns = implode(", ", $columns);
+        //     $sql = "SELECT $columns FROM $table";
+        //     foreach($join as $j){
+        //         $sql .= " JOIN $j";
+        //     }
+        //     $sql .= " ORDER BY $orderBy";
+        //     return $sql;
+        // }
+
+
+
+        public function select(
             string $table,
-            array $columns,
-            array $where,
-            DatabaseCredentials $db
+            array $columns = [],
+            array $where = [],
+            array $join = [],
+            string $orderBy = ""
         ): string {
-            $columns = implode(", ", $columns);
-            $sql = "SELECT $columns FROM $table WHERE $where";
-            return $sql;
-        }
-
-
-
-        public static function selectAll(
-            string $table,
-            DatabaseCredentials $db
-        ): string {
-            $sql = "SELECT * FROM $table";
-            return $sql;
-        }
-
-        public static function selectAllWhere(
-            string $table,
-            array $where,
-            DatabaseCredentials $db
-        ): string {
-            $sql = "SELECT * FROM $table WHERE " . implode(" AND ", $where);
-            return $sql;
-        }
-
-        public static function selectAllOrderBy(
-            string $table,
-            DatabaseCredentials $db,
-            string $orderBy
-        ): string {
-            $sql = "SELECT * FROM $table ORDER BY $orderBy";
-            return $sql;
-        }
-
-        public static function selectAllWhereOrderBy(
-            string $table,
-            array $where,
-            DatabaseCredentials $db,
-            string $orderBy
-        ): string {
-            $sql = "SELECT * FROM $table WHERE $where ORDER BY $orderBy";
-            return $sql;
-        }
-
-
-        public static function selectAllJoin(
-            string $table,
-            array $columns,
-            array $join,
-            DatabaseCredentials $db
-        ): string {
-            $columns = implode(", ", $columns);
-            $sql = "SELECT $columns FROM $table";
-            foreach($join as $j){
+            $sql = "SELECT ";
+            if (empty($columns)) {
+                $sql .= "*";
+            } else {
+                $sql .= implode(", ", $columns);
+            }
+    
+            $sql .= " FROM $table";
+    
+            foreach ($join as $j) {
                 $sql .= " JOIN $j";
             }
-            return $sql;
-        }
-
-        public static function selectAllJoinWhere(
-            string $table,
-            array $columns,
-            array $join,
-            array $where,
-            DatabaseCredentials $db
-        ): string {
-            $columns = implode(", ", $columns);
-            $sql = "SELECT $columns FROM $table";
-            foreach($join as $j){
-                $sql .= " JOIN $j";
+    
+            if (!empty($where)) {
+                $whereClause = implode(" AND ", $where);
+                $sql .= " WHERE $whereClause";
             }
-            $sql .= " WHERE $where";
-            return $sql;
-        }
-
-
-        public static function selectAllJoinWhereOrderBy(
-            string $table,
-            array $columns,
-            array $join,
-            array $where,
-            array $orderBy,
-            DatabaseCredentials $db
-        ): string {
-            $columns = implode(", ", $columns);
-            $sql = "SELECT $columns FROM $table";
-            foreach($join as $j){
-                $sql .= " JOIN $j";
+    
+            if (!empty($orderBy)) {
+                $sql .= " ORDER BY $orderBy";
             }
-            $sql .= " WHERE $where";
-            $sql .= " ORDER BY $orderBy";
+    
             return $sql;
         }
-
+    
     }
---
+++ tests/PhpUnits/Microservices/Database/Helpers/tests.php
@@ -0,0 +1,118 @@
+<?php
+
+
+
+use DigitalSplash\Database\Models\DatabaseCredentials;
+use DigitalSplash\Database\Helpers\QueryBuilder;
+use DigitalSplash\Helpers\Helper;
+
+class Tests{
+
+    public function testInsert() {
+        $table = 'users';
+        $data = [
+            'name' => 'John Doe',
+            'email' => 'john@example.com',
+            'password' => '123456'
+        ];
+        $db = new DatabaseCredentials('localhost', 'root', '', 'test');
+        $sql = QueryBuilder::insert($table, $data, $db);
+        $expectedSql = "INSERT INTO users (name, email, password) VALUES (?, ?, ?)";
+        $this->assertEquals($expectedSql, $sql);
+    }
+
+    public function testUpdate() {
+        $table = 'users';
+        $data = [
+            'name' => 'John',
+            'age' => 25
+        ];
+        $where = 'id = 1';
+        $db = new DatabaseCredentials('localhost', 'root', 'password', 'test');
+        $expectedSql = "UPDATE users SET (name, age) VALUES (?, ?) WHERE id = 1";
+        $actualSql = QueryBuilder::update($table, $data, $where, $db);
+        $this->assertEquals($expectedSql, $actualSql);
+    }
+
+    public function testDelete() {
+        $table = 'users';
+        $where = 'id=1';
+        $db = new DatabaseCredentials('localhost', 'username', 'password', 'database');
+        $expected = "DELETE FROM $table WHERE $where";
+        $actual = QueryBuilder::delete($table, $where, $db);
+        $this->assertEquals($expected, $actual);
+    }
+
+    // public function testSelect() {
+    //     $table = 'users';
+    //     $columns = ['name', 'email'];
+    //     $where = 'id = 1';
+    //     $db = new DatabaseCredentials('localhost', 'root', 'password', 'test_db');
+
+    //     $expectedSql = "SELECT name, email FROM users WHERE id = 1";
+    //     $actualSql = QueryBuilder::select($table, $columns, $where, $db);
+
+    //     $this->assertEquals($expectedSql, $actualSql);
+    // }
+
+    // public function testSelectAll() {
+    //     $table = 'users';
+    //     $db = new DatabaseCredentials('localhost', 'root', 'password', 'test_db');
+    //     $expected = "SELECT * FROM $table";
+    //     $actual = QueryBuilder::selectAll($table, $db);
+    //     $this->assertEquals($expected, $actual);
+    // }
+
+    // public function testSelectAllWhere() {
+    //     $table = 'users';
+    //     $where = ['name' => 'John', 'age' => 25];
+    //     $db = new DatabaseCredentials('localhost', 'root', 'password', 'test_db');
+    //     $expectedSql = "SELECT * FROM users WHERE name='John' AND age=25";
+    //     $actualSql = QueryBuilder::selectAllWhere($table, $where, $db);
+    //     $this->assertEquals($expectedSql, $actualSql);
+    // }
+
+
+    // public function testSelectAllOrderBy() {
+    //     $table = 'users';
+    //     $db = new DatabaseCredentials('localhost', 'root', 'password', 'test_db');
+    //     $orderBy = 'name';
+    //     $expectedSql = "SELECT * FROM $table ORDER BY $orderBy";
+    //     $actualSql = QueryBuilder::selectAllOrderBy($table, $db, $orderBy);
+    //     $this->assertEquals($expectedSql, $actualSql);
+    // }
+
+
+    // public function testSelectAllWhereOrderBy() {
+    //     $table = 'users';
+    //     $where = ['name' => 'John', 'age' => 25];
+    //     $db = new DatabaseCredentials('localhost', 'root', 'password', 'test_db');
+    //     $orderBy = 'name';
+    //     $expectedSql = "SELECT * FROM users WHERE name='John' AND age=25 ORDER BY name";
+    //     $actualSql = QueryBuilder::selectAllWhereOrderBy($table, $where, $db, $orderBy);
+    //     $this->assertEquals($expectedSql, $actualSql);
+    // }
+
+
+    
+    public function testSelectWithEmptyColumnsAndNoWhereAndNoJoinAndNoOrderBy()
+    {
+        $sql = $this->db->select('users');
+        $this->assertEquals('SELECT * FROM users', $sql);
+    }
+    
+    public function testSelectWithColumnsAndNoWhereAndNoJoinAndNoOrderBy()
+    {
+        $sql = $this->db->select('users', ['id', 'name']);
+        $this->assertEquals('SELECT id, name FROM users', $sql);
+    }
+    
+    public function testSelectWithColumnsAndWhereAndNoJoinAndNoOrderBy()
+    {
+        $sql = $this->db->select('users', ['id', 'name'], ['age > 18', 'country = "USA"']);
+        $this->assertEquals('SELECT id, name FROM users WHERE age > 18 AND country = "USA"', $sql);
+    }
+
+
+
+}
