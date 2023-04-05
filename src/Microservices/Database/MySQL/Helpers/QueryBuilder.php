eded1d417ce8418d726eb9bc2e8c7b506f3f2e65
hadi-darwish <hadi.darwish.03@gmail.com>
2023-04-05 05:14:35 +0300

    DG-00007 adding select sql builder

--- src/Microservices/Database/MySQL/Helpers/QueryBuilder.php
+++ src/Microservices/Database/MySQL/Helpers/QueryBuilder.php
@@ -126,6 +126,63 @@ use DigitalSplash\Helpers\Helper;
             ];
         }
 
+        public function select(
+            array $columns = [],
+            array $where = [],
+            array $join,
+            string $orderBy,
+            string $orderType,
+            int $limit,
+            int $offset
+        ): array {
+            $columnsStr = '*';
+            if (!Helper::ArrayNullOrEmpty($columns)) {
+                $columnsStr = Helper::ImplodeArrToStr($columns, ',');
+            }
+
+            $sql = "SELECT $columnsStr FROM {$this->database}.{$this->table}";
+
+            if (!Helper::ArrayNullOrEmpty($join)) {
+                foreach ($join AS $joinTable => $joinData) {
+                    $sql .= " {$joinData['type']} JOIN {$joinTable} ON {$joinData['on']}";
+                }
+            }
+
+            if (!Helper::ArrayNullOrEmpty($where)) {
+                $whereStr = '';
+                foreach ($where AS $column => $value) {
+                    $whereStr .= "`{$column}` = :{$column} AND ";
+                    $bind_key = ':' . $column;
+
+                    $binds[$bind_key] = [
+                        'value' => $value,
+                        'type' => self::GetPDOTypeFromValue($value)
+                    ];
+                }
+                $whereStr = rtrim($whereStr, ' AND ');
+                $sql .= " WHERE $whereStr";
+            }
+
+            if (!empty($orderBy)) {
+                $sql .= " ORDER BY $orderBy $orderType";
+            }
+
+            if (!empty($limit)) {
+                $sql .= " LIMIT $limit";
+            }
+
+            if (!empty($offset)) {
+                $sql .= " OFFSET $offset";
+            }
+
+            return [
+                self::SQL => $sql,
+                self::BINDS => $binds
+            ];
+        }
+
+
+
 		// public function update(
 		// 	string $table,
 		// 	array $data,
