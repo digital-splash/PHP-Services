7aec1845820ef80839c9a2d881767fe1bb28b577
hadi-darwish <hadi.darwish.03@gmail.com>
2023-04-06 08:50:08 +0300

    DG-00007 adding getWhereStatement

--- src/Microservices/Database/MySQL/Helpers/QueryBuilder.php
+++ src/Microservices/Database/MySQL/Helpers/QueryBuilder.php
@@ -192,176 +192,7 @@
 
 
 
-		// public function update(
-		// 	string $table,
-		// 	array $data,
-		// 	array $where,
-		// 	DatabaseCredentials $db
-		// ): string {
-		// 	$columns = array_keys($data);
-		// 	$values = array_values($data);
-		// 	$placeholders = implode(', ', $data);
-		// 	$columns = implode(", ", $columns);
-		// 	$sql = "UPDATE $table SET ($columns) VALUES ($placeholders) WHERE $where";
-		// 	return $sql;
-		// }
-
-		// public  function delete(
-		// 	string $table,
-		// 	array $where,
-		// 	DatabaseCredentials $db
-		// ): string {
-		// 	$sql = "DELETE FROM $table WHERE $where";
-		// 	return $sql;
-		// }
-
-		// public static function select(
-		//     string $table,
-		//     array $columns,
-		//     array $where,
-		//     DatabaseCredentials $db
-		// ): string {
-		//     $columns = implode(", ", $columns);
-		//     $sql = "SELECT $columns FROM $table WHERE $where";
-		//     return $sql;
-		// }
-
-
-
-		// public static function selectAll(
-		//     string $table,
-		//     DatabaseCredentials $db
-		// ): string {
-		//     $sql = "SELECT * FROM $table";
-		//     return $sql;
-		// }
-
-		// public static function selectAllWhere(
-		//     string $table,
-		//     array $where,
-		//     DatabaseCredentials $db
-		// ): string {
-		//     $sql = "SELECT * FROM $table WHERE " . implode(" AND ", $where);
-		//     return $sql;
-		// }
-
-		// public static function selectAllOrderBy(
-		//     string $table,
-		//     DatabaseCredentials $db,
-		//     string $orderBy
-		// ): string {
-		//     $sql = "SELECT * FROM $table ORDER BY $orderBy";
-		//     return $sql;
-		// }
-
-		// public static function selectAllWhereOrderBy(
-		//     string $table,
-		//     array $where,
-		//     string $orderBy
-		// ): string {
-		//     $sql = "SELECT * FROM $table WHERE $where ORDER BY $orderBy";
-		//     return $sql;
-		// }
-
-
-		// public static function selectAllJoin(
-		//     string $table,
-		//     array $columns,
-		//     array $join,
-		// ): string {
-		//     $columns = implode(", ", $columns);
-		//     $sql = "SELECT $columns FROM $table";
-		//     foreach($join as $j){
-		//         $sql .= " JOIN $j";
-		//     }
-		//     return $sql;
-		// }
-
-		// public static function selectAllJoinWhere(
-		//     string $table,
-		//     array $columns,
-		//     array $join,
-		//     array $where,
-		//     DatabaseCredentials $db
-		// ): string {
-		//     $columns = implode(", ", $columns);
-		//     $sql = "SELECT $columns FROM $table";
-		//     foreach($join as $j){
-		//         $sql .= " JOIN $j";
-		//     }
-		//     $sql .= " WHERE $where";
-		//     return $sql;
-		// }
-
-
-		// public static function selectAllJoinWhereOrderBy(
-		//     string $table,
-		//     array $columns,
-		//     array $join,
-		//     array $where,
-		//     array $orderBy,
-		//     DatabaseCredentials $db
-		// ): string {
-		//     $columns = implode(", ", $columns);
-		//     $sql = "SELECT $columns FROM $table";
-		//     foreach($join as $j){
-		//         $sql .= " JOIN $j";
-		//     }
-		//     $sql .= " WHERE $where";
-		//     $sql .= " ORDER BY $orderBy";
-		//     return $sql;
-		// }
-
-		// public static function selectAllJoinOrderBy(
-		//     string $table,
-		//     array $columns,
-		//     array $join,
-		//     array $orderBy,
-		//     DatabaseCredentials $db
-		// ): string {
-		//     $columns = implode(", ", $columns);
-		//     $sql = "SELECT $columns FROM $table";
-		//     foreach($join as $j){
-		//         $sql .= " JOIN $j";
-		//     }
-		//     $sql .= " ORDER BY $orderBy";
-		//     return $sql;
-		// }
-
-
-
-		// public function select(
-		// 	string $table,
-		// 	array $columns = [],
-		// 	array $where = [],
-		// 	array $join = [],
-		// 	string $orderBy = ""
-		// ): string {
-		// 	$sql = "SELECT ";
-		// 	if (empty($columns)) {
-		// 		$sql .= "*";
-		// 	} else {
-		// 		$sql .= implode(", ", $columns);
-		// 	}
-
-		// 	$sql .= " FROM $table";
-
-		// 	foreach ($join as $j) {
-		// 		$sql .= " JOIN $j";
-		// 	}
-
-		// 	if (!empty($where)) {
-		// 		$whereClause = implode(" AND ", $where);
-		// 		$sql .= " WHERE $whereClause";
-		// 	}
-
-		// 	if (!empty($orderBy)) {
-		// 		$sql .= " ORDER BY $orderBy";
-		// 	}
-
-		// 	return $sql;
-		// }
-
+		
 		public static function GetPDOTypeFromValue(
 			mixed $value
 		): int {
@@ -374,5 +205,25 @@
 
 			return $type;
 		}
+
+        public static function getWhereStatement(
+            array $values
+        ): string {
+            if (!Helper::ArrayNullOrEmpty($values)) {
+				$whereStr = '';
+				foreach ($values AS $column => $value) {
+					$whereStr .= "`{$column}` = :{$column} AND ";
+					$bind_key = ':' . $column;
+
+					$binds[$bind_key] = [
+						'value' => $value,
+						'type' => self::GetPDOTypeFromValue($value)
+					];
+				}
+				$whereStr = rtrim($whereStr, ' AND ');
+				return " WHERE $whereStr";
+			}
+            return '';
+        }
 
 	}
