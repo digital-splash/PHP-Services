7723570f1d7d2f5e93a4cc705489a030fe459401
hadi-darwish <hadi.darwish.03@gmail.com>
2023-04-11 17:42:34 +0300

    DG-00009 adding testSetOrder

--- tests/PhpUnits/Microservices/Database/MySQL/Helpers/QueryBuilderTest.php
+++ tests/PhpUnits/Microservices/Database/MySQL/Helpers/QueryBuilderTest.php
@@ -489,6 +489,20 @@ use PHPUnit\Framework\TestCase;
             $queryBuilder = new QueryBuilder($database, $table);
             $this->assertEquals([], $queryBuilder->getOrder());
         }
+
+        public function testSetOrder(): void {
+            $database = 'db';
+            $table = 'table';
+            $order = [
+                'name' => 'ASC',
+                'email' => 'DESC',
+                'age' => 'ASC',
+            ];
+
+            $queryBuilder = new QueryBuilder($database, $table);
+            $queryBuilder->setOrder($order);
+            $this->assertEquals($order, $queryBuilder->getOrder());
+        }
         
 		public function testInsertNoDataToInsertThrows(): void {
 			$this->expectException(NotEmptyParamException::class);
