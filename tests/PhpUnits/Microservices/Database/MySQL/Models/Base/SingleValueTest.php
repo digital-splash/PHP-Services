abf185992581c0f73aaba6f2adca7f44b5b92893
hadi-darwish <hadi.darwish.03@gmail.com>
2023-04-19 01:23:12 +0300

    DG-00012 adding testGetFinalString

---
+++ tests/PhpUnits/Microservices/Database/MySQL/Models/Base/SingleValueTest.php
@@ -0,0 +1,15 @@
+<?php
+	namespace DigitalSplash\Tests\Database\MySQL\Models\Base;
+
+	use DigitalSplash\Database\MySQL\Models\Base\SingleValue;
+	use DigitalSplash\Exceptions\NotEmptyParamException;
+	use DigitalSplash\Language\Helpers\Translate;
+	use PHPUnit\Framework\TestCase;
+
+	class SingleValueTest extends TestCase {
+			
+			public function testGetFinalString(): void {
+				$singleValue = new SingleValue('SET');
+				$this->assertEquals('', $singleValue->getFinalString());
+			}
+	}
