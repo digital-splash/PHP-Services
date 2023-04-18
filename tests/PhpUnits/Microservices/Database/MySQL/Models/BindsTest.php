007498e5ea7c929c27c27e18e58f4a2093dcbadc
hadi-darwish <hadi.darwish.03@gmail.com>
2023-04-19 01:40:34 +0300

    DG-00012 adding testClearBinds

--- tests/PhpUnits/Microservices/Database/MySQL/Models/BindsTest.php
+++ tests/PhpUnits/Microservices/Database/MySQL/Models/BindsTest.php
@@ -4,30 +4,41 @@
 	use DigitalSplash\Database\MySQL\Models\Binds;
 	use DigitalSplash\Exceptions\NotEmptyParamException;
 	use DigitalSplash\Language\Helpers\Translate;
-use PDO;
-use PHPUnit\Framework\TestCase;
+	use PDO;
+	use PHPUnit\Framework\TestCase;
 
-    class BindsTest extends TestCase {
-                
-            public function testGetBinds(): void {
-                $binds = new Binds();
-                $this->assertEqualsCanonicalizing([], $binds->getBinds());
-            }
+	class BindsTest extends TestCase {
+				
+		public function testGetBinds(): void {
+			$binds = new Binds();
+			$this->assertEqualsCanonicalizing([], $binds->getBinds());
+		}
 
-            public function testSetBinds(): void {
-                $binds = new Binds();
-                $binds->setBinds([
-                    ':name_1' => [
-                        'value' => 'John',
-                        'type' => PDO::PARAM_STR
-				    ]
-                ]);
-                $this->assertEqualsCanonicalizing([
-                    ':name_1' => [
-                        'value' => 'John',
-                        'type' => PDO::PARAM_STR
-				    ]
-                ], $binds->getBinds());
-            }
-            
-    }
+		public function testSetBinds(): void {
+			$binds = new Binds();
+			$binds->setBinds([
+				':name_1' => [
+					'value' => 'John',
+					'type' => PDO::PARAM_STR
+				]
+			]);
+			$this->assertEqualsCanonicalizing([
+				':name_1' => [
+					'value' => 'John',
+					'type' => PDO::PARAM_STR
+				]
+			], $binds->getBinds());
+		}
+
+		public function testClearBinds(): void {
+			$binds = new Binds();
+			$binds->setBinds([
+				':name_1' => [
+					'value' => 'John',
+					'type' => PDO::PARAM_STR
+				]
+			]);
+			$binds->clearBinds();
+			$this->assertEqualsCanonicalizing([], $binds->getBinds());
+		}
+	}
