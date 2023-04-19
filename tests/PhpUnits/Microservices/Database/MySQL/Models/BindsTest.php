f3b17644e7b7054ce4c00e72ab5f6d407188755b
hadi-darwish <hadi.darwish.03@gmail.com>
2023-04-20 02:50:16 +0300

    DG-00012 converting bind test to one method

--- tests/PhpUnits/Microservices/Database/MySQL/Models/BindsTest.php
+++ tests/PhpUnits/Microservices/Database/MySQL/Models/BindsTest.php
@@ -6,15 +6,12 @@
 	use PHPUnit\Framework\TestCase;
 
 	class BindsTest extends TestCase {
-				
-		public function testGetBinds(): void {
-			$binds = new Binds();
-			$this->assertEqualsCanonicalizing([], $binds->getBinds());
-		}
 
-		public function testSetBinds(): void {
-			$binds = new Binds();
-			$binds->setBinds([
+        public function testBinds(): void {
+            $binds = new Binds();
+			$this->assertEqualsCanonicalizing([], $binds->getBinds());
+
+            $binds->setBinds([
 				':name_1' => [
 					'value' => 'John',
 					'type' => PDO::PARAM_STR
@@ -26,26 +23,7 @@
 					'type' => PDO::PARAM_STR
 				]
 			], $binds->getBinds());
-		}
 
-		public function testClearBinds(): void {
-			$binds = new Binds();
-			$binds->setBinds([
-				':name_1' => [
-					'value' => 'John',
-					'type' => PDO::PARAM_STR
-				]
-			]);
-			$binds->clearBinds();
-			$this->assertEqualsCanonicalizing([], $binds->getBinds());
-		}
-
-		public function testAppendToBinds(): void {
-			$binds = new Binds();
-			$binds->setBinds([':name_1' => [
-				'value' => 'John',
-				'type' => PDO::PARAM_STR
-			]]);
             $binds->appendToBinds(':name_2', [
                 'value' => 'Jane',
                 'type' => PDO::PARAM_STR
@@ -60,5 +38,8 @@
                     'type' => PDO::PARAM_STR
                 ]
 			], $binds->getBinds());
-		}
+
+            $binds->clearBinds();
+			$this->assertEqualsCanonicalizing([], $binds->getBinds());
+        }
 	}
