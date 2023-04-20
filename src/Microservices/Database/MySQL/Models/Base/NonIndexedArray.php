8915442a87564cf19d4f085a112303806220b47c
hadi-darwish <hadi.darwish.03@gmail.com>
2023-04-20 15:24:03 +0300

    DG-00011 => adding binds to nonIndexedArray

--- src/Microservices/Database/MySQL/Models/Base/NonIndexedArray.php
+++ src/Microservices/Database/MySQL/Models/Base/NonIndexedArray.php
@@ -1,7 +1,8 @@
 <?php
 	namespace DigitalSplash\Database\MySQL\Models\Base;
 
-	use DigitalSplash\Exceptions\NotEmptyParamException;
+    use DigitalSplash\Database\MySQL\Models\Binds;
+    use DigitalSplash\Exceptions\NotEmptyParamException;
 	use DigitalSplash\Helpers\Helper;
 
 	class NonIndexedArray {
@@ -9,6 +10,7 @@
 		private string $statementPrefix;
 		protected string $finalString;
 		protected array $array;
+		public Binds $binds;
 
 		public function __construct(
 			string $implodeValue,
@@ -22,6 +24,7 @@
 			$this->statementPrefix = $statementPrefix;
 			$this->finalString = '';
 			$this->array = [];
+			$this->binds = new Binds();
 		}
 
 		public function getFinalString(): string {
