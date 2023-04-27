f574f94c2bb252bef6ccc9ede786afa51a9b18b6
hadi-darwish <hadi.darwish.03@gmail.com>
2023-04-27 03:04:15 +0300

    DG-00015 => adding DocumentsExtensions model

---
+++ src/Microservices/Media/Models/DocumentsExtensions.php
@@ -0,0 +1,23 @@
+<?php
+	namespace DigitalSplash\Media\Models;
+
+	class DocumentsExtensions {
+
+		public const PDF = "pdf";
+		public const DOC = "doc";
+		public const DOCX = "docx";
+		public const XLS = "xls";
+		public const XLSX = "xlsx";
+
+		public const EXTENSIONS = [
+			self::PDF,
+			self::DOC,
+			self::DOCX,
+			self::XLS,
+			self::XLSX,
+		];
+
+		public function getExtensions(): array {
+			return self::EXTENSIONS;
+		}
+	}
