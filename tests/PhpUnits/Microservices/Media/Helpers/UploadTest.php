7c59f04aae8fbb142526a59d611a9b4a76dd4fc7
hadi-darwish <hadi.darwish.03@gmail.com>
2023-04-27 03:56:59 +0300

    DG-00015 => test of constructor of upload class

---
+++ tests/PhpUnits/Microservices/Media/Helpers/UploadTest.php
@@ -0,0 +1,36 @@
+<?php
+	namespace DigitalSplash\Tests\Media\Helpers;
+
+	use PHPUnit\Framework\TestCase;
+	use DigitalSplash\Media\Helpers\Upload;
+	use DigitalSplash\Media\Models\ImagesExtensions;
+	use DigitalSplash\Media\Models\DocumentsExtensions;
+
+	class UploadTest extends TestCase {
+
+		public function testConstructor(): void {
+			$upload = new Upload();
+			$this->assertEquals("", $upload->_name);
+			$this->assertEquals("", $upload->_type);
+			$this->assertEquals("", $upload->_tmp_name);
+			$this->assertEquals(0, $upload->_error);
+			$this->assertEquals(0, $upload->_size);
+			$this->assertEquals("", $upload->fileFullPath);
+			$this->assertEquals("", $upload->elemName);
+			$this->assertEquals("", $upload->uploadPath);
+			$this->assertEquals("", $upload->folders);
+			$this->assertEquals("", $upload->destName);
+			$this->assertEquals(ImagesExtensions::getExtensions(), $upload->allowedExtensions);
+			$this->assertEquals(0, $upload->ratio);
+			$this->assertEquals(Upload::convertToNextGen, $upload->convertToNextGen);
+			$this->assertEquals(true, $upload->resize);
+			$this->assertEquals([], $upload->retArr);
+			$this->assertEquals([], $upload->uploadedPaths);
+			$this->assertEquals([], $upload->uploadedData);
+			$this->assertEquals([], $upload->successArr);
+			$this->assertEquals([], $upload->errorArr);
+			$this->assertEquals(0, $upload->error);
+			$this->assertEquals(false, $upload->isTest);
+
+		}
+	}
