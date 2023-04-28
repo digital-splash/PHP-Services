6182674deed197fb07ac98e3a67092c8fbf6532c
hadi-darwish <hadi.darwish.03@gmail.com>
2023-04-29 02:52:36 +0300

    DG-00015 => adding getters and setters of private variables

--- src/Microservices/Media/Helpers/Upload.php
+++ src/Microservices/Media/Helpers/Upload.php
@@ -9,11 +9,11 @@
 		public const Error = 0;
 		public const Success = 1;
 
-		// public $_name;
-		// public $_type;
-		// public $_tmp_name;
-		// public $_error;
-		// public $_size;
+		private $_name;
+		private $_type;
+		private $_tmp_name;
+		private $_error;
+		private $_size;
 
 		// public $fileFullPath;
 
@@ -34,11 +34,11 @@
 		// public $isTest;
 
 		public function __construct() {
-			// $this->_name = "";
-			// $this->_type = "";
-			// $this->_tmp_name = "";
-			// $this->_error = 0;
-			// $this->_size = 0;
+			$this->_name = "";
+			$this->_type = "";
+			$this->_tmp_name = "";
+			$this->_error = 0;
+			$this->_size = 0;
 			// $this->fileFullPath = "";
 			// $this->elemName	 = "";
 			// $this->uploadPath = "";
@@ -56,6 +56,47 @@
 			// $this->error = 0;
 			// $this->isTest = false;
 		}
+
+		public function setName(string $name): void {
+			$this->_name = $name;
+		}
+
+		public function getName(): string {
+			return $this->_name;
+		}
+
+		public function setType(string $type): void {
+			$this->_type = $type;
+		}
+
+		public function getType(): string {
+			return $this->_type;
+		}
+
+		public function setTmpName(string $tmpName): void {
+			$this->_tmp_name = $tmpName;
+		}
+
+		public function getTmpName(): string {
+			return $this->_tmp_name;
+		}
+
+		public function setError(int $error): void {
+			$this->_error = $error;
+		}
+
+		public function getError(): int {
+			return $this->_error;
+		}
+
+		public function setSize(int $size): void {
+			$this->_size = $size;
+		}
+
+		public function getSize(): int {
+			return $this->_size;
+		}
+
 
 		public static function uploadToServer($tmpName="", $uploadPath="", $fileName=""): array {
 			$uploadedFileName	= pathinfo($uploadPath, PATHINFO_BASENAME);
