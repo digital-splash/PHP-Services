d45e68537bec55694da430fc5b63b7c1cc248e73
Mario Rawady <rawady_mario@hotmail.com>
2023-05-10 09:38:52 +0300

    Fix Unit Test

--- tests/PhpUnits/Microservices/Media/Models/FilesTest.php
+++ tests/PhpUnits/Microservices/Media/Models/FilesTest.php
@@ -21,7 +21,6 @@
 						]
 					],
 					'expected' => [
-						// new File('file', 'file1.txt', 'text/plain', '/tmp/phpD567.tmp', UPLOAD_ERR_OK, 1024)
 						'file' => [
 							'name' => 'file1.txt',
 							'type' => 'text/plain',
@@ -308,46 +307,33 @@
 			$this->assertEqualsCanonicalizing($expected, $files_class->toArray());
 		}
 
-		public function buildFilesAllTypesProvider(): array {
-			return [
+		public function testBuildFilesAllTypes(): void {
+			$phpFiles = [
 				'single_file_one_level' => [
-					'files' => [
-						'file' => [
-							'name' => 'file1.txt',
-							'type' => 'text/plain',
-    						'tmp_name' => '/tmp/phpD567.tmp',
-							'error' => UPLOAD_ERR_OK,
-							'size' => 1024
-						]
-					],
-					'expected' => [
-						new File('file', 'file1.txt', 'text/plain', '/tmp/phpD567.tmp', UPLOAD_ERR_OK, 1024)
-					]
+					'name' => 'file1.txt',
+					'type' => 'text/plain',
+					'tmp_name' => '/tmp/phpD567.tmp',
+					'error' => UPLOAD_ERR_OK,
+					'size' => 1024
 				],
 				'multiple_files_one_level' => [
-					'files' => [
-						'files' => [
-							'name' => ['file1.txt', 'file2.png', 'file3.jpg'],
-							'type' => ['text/plain', ImagesExtensions::PNG, ImagesExtensions::JPG],
-							'size' => [1024, 2048, 3072],
-							'tmp_name' => ['/tmp/phpABC123', '/tmp/phpDEF456', '/tmp/phpGHI789'],
-							'error' => [UPLOAD_ERR_OK, UPLOAD_ERR_OK, UPLOAD_ERR_OK]
-						]
-					],
-					'expected' =>[
-						new File('[files][0]', 'file1.txt', 'text/plain', '/tmp/phpABC123', UPLOAD_ERR_OK, 1024),
-						new File('[files][1]', 'file2.png', ImagesExtensions::PNG, '/tmp/phpDEF456', UPLOAD_ERR_OK, 2048),
-						new File('[files][2]', 'file3.jpg', ImagesExtensions::JPG, '/tmp/phpGHI789', UPLOAD_ERR_OK, 3072)
-					]
+					'name' => ['file1.txt', 'file2.png', 'file3.jpg'],
+					'type' => ['text/plain', ImagesExtensions::PNG, ImagesExtensions::JPG],
+					'size' => [1024, 2048, 3072],
+					'tmp_name' => ['/tmp/phpABC123', '/tmp/phpDEF456', '/tmp/phpGHI789'],
+					'error' => [UPLOAD_ERR_OK, UPLOAD_ERR_OK, UPLOAD_ERR_OK]
 				]
-			];
-		}
 
-		/**
-		 * @dataProvider buildFilesAllTypesProvider
-		 */
-		public function testBuildFilesAllTypes(array $files, array $expected): void {
-			$files_class = new Files($files);
+			];
+
+			$expected = [
+				new File('single_file_one_level', 'file1.txt', 'text/plain', '/tmp/phpD567.tmp', UPLOAD_ERR_OK, 1024),
+				new File('[multiple_files_one_level][0]', 'file1.txt', 'text/plain', '/tmp/phpABC123', UPLOAD_ERR_OK, 1024),
+				new File('[multiple_files_one_level][1]', 'file2.png', ImagesExtensions::PNG, '/tmp/phpDEF456', UPLOAD_ERR_OK, 2048),
+				new File('[multiple_files_one_level][2]', 'file3.jpg', ImagesExtensions::JPG, '/tmp/phpGHI789', UPLOAD_ERR_OK, 3072)
+			];
+
+			$files_class = new Files($phpFiles);
 			$files_class->buildFiles();
 
 			$this->assertEquals($expected, $files_class->getFiles());
