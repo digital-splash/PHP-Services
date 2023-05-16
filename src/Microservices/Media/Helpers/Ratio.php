9f5acdca50937ef07ce9841faff0c2146e497adf
Mario Rawady <rawady_mario@hotmail.com>
2023-05-16 10:26:08 +0300

    Add comments

--- src/Microservices/Media/Helpers/Ratio.php
+++ src/Microservices/Media/Helpers/Ratio.php
@@ -1,12 +1,25 @@
 <?php
 	namespace DigitalSplash\Media\Helpers;
 
+	/**
+	 * Image Manager Basic Usage: https://image.intervention.io/v2/usage/overview#basic-usage
+	 */
 	use Intervention\Image\ImageManager;
 
 
 	class Ratio {
 		private string $source;
 		private float $ratio;
+
+		//https://stackoverflow.com/questions/44350072/add-white-space-to-image-using-laravel-5-intervention-image-to-make-square-image
+
+		//Add "destination" string: default empty
+		//Add "addCanvas" boolean: default false
+
+		//If destination is not given, set it to be same as source.
+			//Check if destination does not exist, then we need to create an empty image...
+
+		//If canvas is given, we should add white borders to the top/bot or right/left
 
 		public function __construct(
 			string $source,
@@ -36,6 +49,7 @@
 				$newWidth = $width;
 				$newHeight = $width / $this->ratio;
 			}
+
 			$image->resizeCanvas($newWidth, $newHeight, 'center', false, '#ffffff');
 			$image->save($this->source);
 		}
