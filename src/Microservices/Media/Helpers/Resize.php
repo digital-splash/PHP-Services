618a4579205fe463b19b2f2612d066bb8ceb33e3
hadi-darwish <hadi.darwish.03@gmail.com>
2023-05-17 12:34:22 +0300

    DG-00018 => adding Resize class

---
+++ src/Microservices/Media/Helpers/Resize.php
@@ -0,0 +1,36 @@
+<?php
+	namespace DigitalSplash\Media\Helpers;
+
+	/**
+	 * Image Manager Basic Usage: https://image.intervention.io/v2/usage/overview#basic-usage
+	 */
+
+	use Intervention\Image\ImageManager;
+
+    class Resize {
+
+        private string $source;
+        private string $destination;
+        private int $width;
+
+        public function __construct(
+            string $source,
+            string $destination = '',
+            int $width
+        ) {
+            $this->source = $source;
+            $this->destination = $destination;
+            $this->width = $width;
+        }
+
+        public function resize(): void {
+            $manager = new ImageManager([
+				'driver' => 'gd'
+			]);
+            $image = $manager->make($this->source);
+            $image->resize($this->width, null, function ($constraint) {
+                $constraint->aspectRatio();
+            });
+            $image->save($this->destination);
+        }
+    }
