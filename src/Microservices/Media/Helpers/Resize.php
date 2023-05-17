c848fbd2f9f3b7df9742359c5850da2976e4f577
hadi-darwish <hadi.darwish.03@gmail.com>
2023-05-17 14:44:08 +0300

    DG-00018 => adding Resize class

--- src/Microservices/Media/Helpers/Resize.php
+++ src/Microservices/Media/Helpers/Resize.php
@@ -5,32 +5,35 @@
 	 * Image Manager Basic Usage: https://image.intervention.io/v2/usage/overview#basic-usage
 	 */
 
+	use DigitalSplash\Helpers\Helper;
 	use Intervention\Image\ImageManager;
 
-    class Resize {
+	class Resize {
 
-        private string $source;
-        private string $destination;
-        private int $width;
+		private string $source;
+		private string $destination;
+		private int $width;
 
-        public function __construct(
-            string $source,
-            string $destination = '',
-            int $width
-        ) {
-            $this->source = $source;
-            $this->destination = $destination;
-            $this->width = $width;
-        }
+		public function __construct(
+			string $source,
+			string $destination,
+			int $width
+		) {
+			$this->source = $source;
+			$this->destination = $destination;
+			$this->width = $width;
+		}
 
-        public function resize(): void {
-            $manager = new ImageManager([
+		public function resize(): void {
+			$manager = new ImageManager([
 				'driver' => 'gd'
 			]);
-            $image = $manager->make($this->source);
-            $image->resize($this->width, null, function ($constraint) {
-                $constraint->aspectRatio();
-            });
-            $image->save($this->destination);
-        }
-    }
+			$image = $manager->make($this->source);
+			$image->resize($this->width, null, function ($constraint) {
+				$constraint->aspectRatio();
+			});
+
+			Helper::CreateFolderRecursive(pathinfo($this->destination, PATHINFO_DIRNAME));
+			$image->save($this->destination);
+		}
+	}
