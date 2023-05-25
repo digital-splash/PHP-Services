9f0dcc7113736fef419fa1f372b1f2c9e00e7fec
Mario Rawady <rawady_mario@hotmail.com>
2023-05-25 10:09:10 +0300

    Fix Upload Queue Test

--- tests/PhpUnits/Microservices/Media/Helpers/UploadQueueTest.php
+++ tests/PhpUnits/Microservices/Media/Helpers/UploadQueueTest.php
@@ -7,61 +7,65 @@
 	use DigitalSplash\Media\Helpers\UploadQueue;
 
 	class UploadQueueTest extends TestCase {
+		private const DIR = __DIR__ . "/../../../../_CommonFiles/Media/users/profile";
 
 		public function testProcessImages(): void {
-			Helper::CreateFolderRecursive(__DIR__ . "/../../../../_CommonFiles/Media/users/profile/og");
+			Helper::CreateFolderRecursive(self::DIR . "/og");
 			copy(
-				__DIR__ . "/../../../../_CommonFiles/Media/users/profile/user-01.jpg",
-				__DIR__ . "/../../../../_CommonFiles/Media/users/profile/og/user-01.jpg"
+				self::DIR . "/user-01.jpg",
+				self::DIR . "/og/user-01.jpg"
 			);
 			copy(
-				__DIR__ . "/../../../../_CommonFiles/Media/users/profile/user-01-th.jpg",
-				__DIR__ . "/../../../../_CommonFiles/Media/users/profile/og/user-01-th.jpg"
+				self::DIR . "/user-01-th.jpg",
+				self::DIR . "/og/user-01-th.jpg"
 			);
-			Media::SetUploadDir(__DIR__ . "/../../../../_CommonFiles/Media/users/profile/og");
+			Media::SetUploadDir(self::DIR . "/og");
 
 			$uploadQueue = new UploadQueue($_FILES, 'test-upload-queue', '///', [], 5, true, true, ['all']);
-				$uploadQueue->processImages([
-					__DIR__ . "/../../../../_CommonFiles/Media/users/profile/og/user-01.jpg",
-					__DIR__ . "/../../../../_CommonFiles/Media/users/profile/og/user-01-th.jpg",
-				]);
-				$this->assertFileExists(__DIR__ . "/../../../../_CommonFiles/Media/users/profile/og/hd/user-01-th.webp");
-				unlink(__DIR__ . "/../../../../_CommonFiles/Media/users/profile/og/hd/user-01-th.webp");
-				$this->assertFileExists(__DIR__ . "/../../../../_CommonFiles/Media/users/profile/og/hd/user-01.webp");
-				unlink(__DIR__ . "/../../../../_CommonFiles/Media/users/profile/og/hd/user-01.webp");
-				$this->assertFileExists(__DIR__ . "/../../../../_CommonFiles/Media/users/profile/og/ld/user-01-th.webp");
-				unlink(__DIR__ . "/../../../../_CommonFiles/Media/users/profile/og/ld/user-01-th.webp");
-				$this->assertFileExists(__DIR__ . "/../../../../_CommonFiles/Media/users/profile/og/ld/user-01.webp");
-				unlink(__DIR__ . "/../../../../_CommonFiles/Media/users/profile/og/ld/user-01.webp");
-				$this->assertFileExists(__DIR__ . "/../../../../_CommonFiles/Media/users/profile/og/th/user-01-th.webp");
-				unlink(__DIR__ . "/../../../../_CommonFiles/Media/users/profile/og/th/user-01-th.webp");
-				$this->assertFileExists(__DIR__ . "/../../../../_CommonFiles/Media/users/profile/og/th/user-01.webp");
-				unlink(__DIR__ . "/../../../../_CommonFiles/Media/users/profile/og/th/user-01.webp");
-				$this->assertFileExists(__DIR__ . "/../../../../_CommonFiles/Media/users/profile/og/fb/cover/user-01-th.webp");
-				unlink(__DIR__ . "/../../../../_CommonFiles/Media/users/profile/og/fb/cover/user-01-th.webp");
-				$this->assertFileExists(__DIR__ . "/../../../../_CommonFiles/Media/users/profile/og/fb/cover/user-01.webp");
-				unlink(__DIR__ . "/../../../../_CommonFiles/Media/users/profile/og/fb/cover/user-01.webp");
-				$this->assertFileExists(__DIR__ . "/../../../../_CommonFiles/Media/users/profile/og/fb/post/user-01-th.webp");
-				unlink(__DIR__ . "/../../../../_CommonFiles/Media/users/profile/og/fb/post/user-01-th.webp");
-				$this->assertFileExists(__DIR__ . "/../../../../_CommonFiles/Media/users/profile/og/fb/post/user-01.webp");
-				unlink(__DIR__ . "/../../../../_CommonFiles/Media/users/profile/og/fb/post/user-01.webp");
-				$this->assertFileExists(__DIR__ . "/../../../../_CommonFiles/Media/users/profile/og/fb/profile/user-01-th.webp");
-				unlink(__DIR__ . "/../../../../_CommonFiles/Media/users/profile/og/fb/profile/user-01-th.webp");
-				$this->assertFileExists(__DIR__ . "/../../../../_CommonFiles/Media/users/profile/og/fb/profile/user-01.webp");
-				unlink(__DIR__ . "/../../../../_CommonFiles/Media/users/profile/og/fb/profile/user-01.webp");
-				$this->assertFileExists(__DIR__ . "/../../../../_CommonFiles/Media/users/profile/og/user-01-th.webp");
-				unlink(__DIR__ . "/../../../../_CommonFiles/Media/users/profile/og/user-01-th.webp");
-				$this->assertFileExists(__DIR__ . "/../../../../_CommonFiles/Media/users/profile/og/user-01.webp");
-				unlink(__DIR__ . "/../../../../_CommonFiles/Media/users/profile/og/user-01.webp");
+			$uploadQueue->processImages([
+				self::DIR . "/og/user-01.jpg",
+				self::DIR . "/og/user-01-th.jpg",
+			]);
 
-				//remove all created directories
-				rmdir(__DIR__ . "/../../../../_CommonFiles/Media/users/profile/og/hd");
-				rmdir(__DIR__ . "/../../../../_CommonFiles/Media/users/profile/og/ld");
-				rmdir(__DIR__ . "/../../../../_CommonFiles/Media/users/profile/og/th");
-				rmdir(__DIR__ . "/../../../../_CommonFiles/Media/users/profile/og/fb/cover");
-				rmdir(__DIR__ . "/../../../../_CommonFiles/Media/users/profile/og/fb/post");
-				rmdir(__DIR__ . "/../../../../_CommonFiles/Media/users/profile/og/fb/profile");
-				rmdir(__DIR__ . "/../../../../_CommonFiles/Media/users/profile/og/fb");
-				rmdir(__DIR__ . "/../../../../_CommonFiles/Media/users/profile/og");
+			$this->assertFileExists(self::DIR . "/og/hd/user-01-th.webp");
+			Helper::DeleteFileOrFolder(self::DIR . "/og/hd/user-01-th.webp");
+			$this->assertFileExists(self::DIR . "/og/hd/user-01.webp");
+			Helper::DeleteFileOrFolder(self::DIR . "/og/hd/user-01.webp");
+			$this->assertFileExists(self::DIR . "/og/ld/user-01-th.webp");
+			Helper::DeleteFileOrFolder(self::DIR . "/og/ld/user-01-th.webp");
+			$this->assertFileExists(self::DIR . "/og/ld/user-01.webp");
+			Helper::DeleteFileOrFolder(self::DIR . "/og/ld/user-01.webp");
+			$this->assertFileExists(self::DIR . "/og/th/user-01-th.webp");
+			Helper::DeleteFileOrFolder(self::DIR . "/og/th/user-01-th.webp");
+			$this->assertFileExists(self::DIR . "/og/th/user-01.webp");
+			Helper::DeleteFileOrFolder(self::DIR . "/og/th/user-01.webp");
+			$this->assertFileExists(self::DIR . "/og/fb/cover/user-01-th.webp");
+			Helper::DeleteFileOrFolder(self::DIR . "/og/fb/cover/user-01-th.webp");
+			$this->assertFileExists(self::DIR . "/og/fb/cover/user-01.webp");
+			Helper::DeleteFileOrFolder(self::DIR . "/og/fb/cover/user-01.webp");
+			$this->assertFileExists(self::DIR . "/og/fb/post/user-01-th.webp");
+			Helper::DeleteFileOrFolder(self::DIR . "/og/fb/post/user-01-th.webp");
+			$this->assertFileExists(self::DIR . "/og/fb/post/user-01.webp");
+			Helper::DeleteFileOrFolder(self::DIR . "/og/fb/post/user-01.webp");
+			$this->assertFileExists(self::DIR . "/og/fb/profile/user-01-th.webp");
+			Helper::DeleteFileOrFolder(self::DIR . "/og/fb/profile/user-01-th.webp");
+			$this->assertFileExists(self::DIR . "/og/fb/profile/user-01.webp");
+			Helper::DeleteFileOrFolder(self::DIR . "/og/fb/profile/user-01.webp");
+			$this->assertFileExists(self::DIR . "/og/user-01-th.webp");
+			Helper::DeleteFileOrFolder(self::DIR . "/og/user-01-th.webp");
+			$this->assertFileExists(self::DIR . "/og/user-01.webp");
+			Helper::DeleteFileOrFolder(self::DIR . "/og/user-01.webp");
+
+			//remove all created directories
+			Helper::DeleteFileOrFolder(self::DIR . "/og/hd");
+			Helper::DeleteFileOrFolder(self::DIR . "/og/ld");
+			Helper::DeleteFileOrFolder(self::DIR . "/og/th");
+			Helper::DeleteFileOrFolder(self::DIR . "/og/fb/cover");
+			Helper::DeleteFileOrFolder(self::DIR . "/og/fb/post");
+			Helper::DeleteFileOrFolder(self::DIR . "/og/fb/profile");
+			Helper::DeleteFileOrFolder(self::DIR . "/og/fb");
+			Helper::DeleteFileOrFolder(self::DIR . "/og");
+
+			//TODO: Maybe Create a new Function Helper::DeleteFolderAndAllFiles(), that will loop through all the files and folder inside the given folder and deletes them and then delete the fiver folder.
 		}
 	}
