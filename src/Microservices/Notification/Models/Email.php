f9727e6ce4b496f553588a1693d5ea4c8c4fdb26
hadi-darwish <hadi.darwish.03@gmail.com>
2023-05-29 11:59:40 +0300

    DG-00026 => adding setters and getters and appends to necessary variables in Email Model class

--- src/Microservices/Notification/Models/Email.php
+++ src/Microservices/Notification/Models/Email.php
@@ -25,9 +25,9 @@
 
 		private string $subject;
 		private string $body;
+		private $attachments = [];
 
 		// private $templateData = [];
-		// private $attachments = [];
 		// private $mainTemplate = "main.boxed_with_button";
 		// private $template = "";
 
@@ -36,7 +36,7 @@
 			$this->testEmail = 'testing@dgsplash.com';
 		}
 
-		public function appendTo(string $name, string $email) {
+		public function appendTo(string $name, string $email): void {
 			$this->to[] = new Recipient($name, $email);
 		}
 
@@ -47,4 +47,66 @@
 			return $this->to;
 		}
 
+		public function appendCC(string $name, string $email): void {
+			$this->cc[] = new Recipient($name, $email);
+		}
+
+		/**
+		 * @return Recipient[]
+		 */
+		public function getCC(): array {
+			return $this->cc;
+		}
+
+		public function appendBCC(string $name, string $email): void {
+			$this->bcc[] = new Recipient($name, $email);
+		}
+		/**
+		 * @return Recipient[]
+		 */
+		public function getBCC(): array {
+			return $this->bcc;
+		}
+
+		public function setSubject(string $subject): void {
+			$this->subject = $subject;
+		}
+		public function getSubject(): string {
+			return $this->subject;
+		}
+
+		public function setBody(string $body): void {
+			$this->body = $body;
+		}
+		public function getBody(): string {
+			return $this->body;
+		}
+
+		public function setAttachments(array $attachments): void {
+			$this->attachments = $attachments;
+		}
+		public function getAttachments(): array {
+			return $this->attachments;
+		}
+		public function appendToAttachment(string $path, string $name = null): void {
+			$this->attachments[] = [
+				'path' => $path,
+				'name' => $name
+			];
+		}
+
+		public function setIsProd(bool $isProd): void {
+			$this->isProd = $isProd;
+		}
+		public function getIsProd(): bool {
+			return $this->isProd;
+		}
+
+
+		public function setTestEmail(string $testEmail): void {
+			$this->testEmail = $testEmail;
+		}
+		public function getTestEmail(): string {
+			return $this->testEmail;
+		}
 	}
