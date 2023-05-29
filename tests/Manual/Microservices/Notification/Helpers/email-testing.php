19f4418f4de7b8778a41e0d6357f39690e1417bd
hadi-darwish <hadi.darwish.03@gmail.com>
2023-05-29 16:17:35 +0300

    DG-00026 => adding tests

--- tests/Manual/Microservices/Notification/Helpers/email-testing.php
+++ tests/Manual/Microservices/Notification/Helpers/email-testing.php
@@ -1,8 +1,24 @@
 <?php
 	use DigitalSplash\Notification\Helpers\Email\Email;
+	use DigitalSplash\Notification\Models\EmailConfiguration;
 
 	include_once __DIR__ . '/../../../../../vendor/autoload.php';
 
+	EmailConfiguration::setHost('Put Host Here');
+	EmailConfiguration::setPort(Put Port here);
+	EmailConfiguration::setUsername('Put username here');
+	EmailConfiguration::setPassword('Put password here');
+	EmailConfiguration::setEncryption('Put wncryption here');
+	EmailConfiguration::setFromName('Testing Email');
+
+
 	$email = new Email();
-	$email->model->appendTo('Testing Email', 'testing@dgsplash.com');
+	$email->model->setTestEmail('Put testing email');
+	$email->model->appendTo('Testing Email', 'add a recipient ');
+	$email->model->appendTo('Hadi Darwish', 'add a recipient ');
+	$email->model->setSubject('Testing Email');
+	$email->model->setBody('This is a test email');
+	$email->model->appendCC('Testing Email','add cc');
+	$email->model->appendBCC('Hadi Darwish','add bcc');
+
 	$email->send();
