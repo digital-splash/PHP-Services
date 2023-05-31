0b46e0ac41cbffd004b62a6c381453662cb04011
hadi-darwish <hadi.darwish.03@gmail.com>
2023-05-31 23:31:01 +0300

    DG-00028 => Initial Commit Adding Template Model

---
+++ src/Microservices/Notification/Models/Template.php
@@ -0,0 +1,19 @@
+<?php
+	namespace DigitalSplash\Notification\Models;
+
+	use DigitalSplash\Helpers\Helper;
+
+	class Template {
+		const TEMPLATE_PATH = __DIR__ . '/../Templates/Email/';
+
+		private $keys = [];
+
+		public function __construct(array $keys) {
+			$this->keys = $keys;
+		}
+
+		public function getContent(string $templateKey, array $replace = []): string {
+			$templatePath = Helper::RemoveMultipleSlashesInUrl(self::TEMPLATE_PATH . $templateKey . '.html');
+			return Helper::getContentFromFile($templatePath, $replace);
+		}
+	}
