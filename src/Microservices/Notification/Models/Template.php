a821c23349c1a12351291824d81d64e7040389ca
hadi-darwish <hadi.darwish.03@gmail.com>
2023-06-02 14:30:10 +0300

    DG-00028 => adding setters and getters

--- src/Microservices/Notification/Models/Template.php
+++ src/Microservices/Notification/Models/Template.php
@@ -4,18 +4,49 @@
 	use DigitalSplash\Helpers\Helper;
 
 	class Template {
-		// const TEMPLATE_PATH = __DIR__ . '../Templates/Email/';
+		const TEMPLATE_PATH = __DIR__ . '/../Templates/Email/';
 
 		private $keys = [];
+		private $templatePath = '';
+		private $templateContentPath = '';
 
-		public function __construct(array $keys) {
+		public function __construct(
+			array $keys,
+			string $templatePath,
+			string $templateContentPath
+			) {
+			$this->keys = $keys;
+			$this->templatePath = Helper::RemoveMultipleSlashesInUrl(self::TEMPLATE_PATH . $templatePath . '.html');
+			$this->templateContentPath = Helper::RemoveMultipleSlashesInUrl(self::TEMPLATE_PATH . $templateContentPath . '.html');
+		}
+
+		public function getKeys(): array {
+			return $this->keys;
+		}
+
+		public function setKeys(array $keys): void {
 			$this->keys = $keys;
 		}
 
-		public function getContent(string $templateKey, array $replace = []): string {
-			// $templatePath = Helper::RemoveMultipleSlashesInUrl(self::TEMPLATE_PATH . $templateKey . '.html');
-			$templatePath = Helper::RemoveMultipleSlashesInUrl($templateKey);
-			echo $templatePath;
-			return Helper::getContentFromFile($templatePath, $replace);
+		public function appendKey(string $key, string $value): void {
+			$this->keys[$key] = $value;
 		}
+
+		public function getTemplatePath(): string {
+			return $this->templatePath;
+		}
+
+		public function setTemplatePath(string $templatePath): void {
+			$this->templatePath = $templatePath;
+		}
+
+		public function getTemplateContentPath(): string {
+			return $this->templateContentPath;
+		}
+
+		public function setTemplateContentPath(string $templateContentPath): void {
+			$this->templateContentPath = $templateContentPath;
+		}
+
+		
 	}
