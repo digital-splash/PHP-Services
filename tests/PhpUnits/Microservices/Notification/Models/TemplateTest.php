ed5c18bd91ec3884dba2345da58a643b62d8ecb8
hadi-darwish <hadi.darwish.03@gmail.com>
2023-06-02 16:49:46 +0300

    DG-00028 => adding testGetContentSuccess

--- tests/PhpUnits/Microservices/Notification/Models/TemplateTest.php
+++ tests/PhpUnits/Microservices/Notification/Models/TemplateTest.php
@@ -1,8 +1,8 @@
 <?php
 	namespace DigitalSplash\Tests\Notification\Models;
 
-use DigitalSplash\Helpers\Helper;
-use DigitalSplash\Notification\Models\Template;
+	use DigitalSplash\Helpers\Helper;
+	use DigitalSplash\Notification\Models\Template;
 	use PHPUnit\Framework\TestCase;
 
 	class TemplateTest extends TestCase {
@@ -10,26 +10,26 @@ use DigitalSplash\Notification\Models\Template;
 		public function templateSuccessProvider(): array {
 			return [
 				'Main/boxed_with_button' => [
-					'path' => 'Main/boxed_with_button',
+					'path' => 'boxed_with_button',
 					'email_content' => 'TestEmail',
 					'keys' => [
-						'{{full_name}}' => 'Hadi Darwish',
-						'{{tenant_name}}' => 'Digital Splash',
-						'{{url}}' => 'dgsplash.com',
-						'{{tenant_main_color}}' => '#0000ff',
-						'{{button_text}}' => 'Test button',
-						'{{tenant_year}}' => '2023',
-						'{{tenant_logo}}' => "https://dgsplash.com/assets/images/logo-bg.jpg"
+						'full_name' => 'Hadi Darwish',
+						'tenant_name' => 'Digital Splash',
+						'url' => 'dgsplash.com',
+						'tenant_main_color' => '#0000ff',
+						'button_text' => 'Test button',
+						'tenant_year' => '2023',
+						'tenant_logo' => "https://dgsplash.com/assets/images/logo-bg.jpg"
 					]
 				],
 				'Main/boxed' => [
-					'path' => 'Main/boxed',
+					'path' => 'boxed',
 					'email_content' => 'TestEmail',
 					'keys' => [
-						'{{full_name}}' => 'Hadi Darwish',
-						'{{tenant_name}}' => 'Digital Splash',
-						'{{tenant_year}}' => '2023',
-						'{{tenant_logo}}' => "https://dgsplash.com/assets/images/logo-bg.jpg"
+						'full_name' => 'Hadi Darwish',
+						'tenant_name' => 'Digital Splash',
+						'tenant_year' => '2023',
+						'tenant_logo' => "https://dgsplash.com/assets/images/logo-bg.jpg"
 					]
 				],
 			];
@@ -38,9 +38,14 @@ use DigitalSplash\Notification\Models\Template;
 		/**
 		 * @dataProvider templateSuccessProvider
 		 */
-		public function testGetContentSuccess(string $path, string $email_content, array $keys): void {
-			$template = new Template($keys,$path,$email_content);
+		public function testGetContentSuccess(string $path, string $emailContent, array $keys): void {
+			$template = new Template($keys, $path, $emailContent);
 			$templateContent = $template->getContent();
-			$this->assertEquals(Helper::GetContentFromFile(__DIR__ . '/../../../../_CommonFiles/Notification/Email/' . $path . '.html'), $templateContent);
+			$this->assertEquals(
+				Helper::GetContentFromFile(
+					__DIR__ . '/../../../../_CommonFiles/Notification/Email/Main/' . $path . '.html'
+				),
+				$templateContent
+			);
 		}
 	}
