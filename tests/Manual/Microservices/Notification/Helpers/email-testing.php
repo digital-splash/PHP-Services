7d845513ea7f8322adef0cde0e61740a4c159e24
hadi-darwish <hadi.darwish.03@gmail.com>
2023-06-09 20:22:59 +0300

    DG-00033 => testing

--- tests/Manual/Microservices/Notification/Helpers/email-testing.php
+++ tests/Manual/Microservices/Notification/Helpers/email-testing.php
@@ -1,9 +1,13 @@
 <?php
+	ini_set('display_errors', '1');
+	error_reporting(E_ALL);
 	use DigitalSplash\Notification\Helpers\Notification;
 	use DigitalSplash\Notification\Models\Template;
 
 	include_once __DIR__ . '/../../../../../vendor/autoload.php';
 
+	echo xdebug_info();
+	echo 1 + '1a';
 
 	$template = new Template(
 		[
@@ -26,10 +30,12 @@
 	$notification->model->appendToAttachment(__DIR__ . "/../../../../_CommonFiles/Media/users/profile/user-01.jpg", 'user-01.jpg');
 	$notification->model->appendToAttachment(__DIR__ . "/../../../../_CommonFiles/Media/users/profile/user-01-th.jpg", 'user-01-th.jpg');
 	$notification->model->appendToAttachment(__DIR__ . "/../../../../_CommonFiles/Media/users/profile/user-01.webp", 'user-01.webp');
-	$notification->model->setSubject('Testing Email');
-	$notification->model->email->setBody(
-		$template->getContent()
-	);
+	// $notification->model->setSubject('Testing Email');
+	$notification->model->setSubject('');
+	// $notification->model->email->setBody(
+	// 	$template->getFullEmailHtmlBoxedWithButton()
+	// );
+	$notification->model->email->setBody('');
 
 	$notificationResponse = $notification->send();
 	var_dump($notificationResponse);
