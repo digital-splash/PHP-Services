35e966f505c57044c10676aa6e5dfcc32c5b2f0c
Mario Rawady <rawady_mario@hotmail.com>
2023-09-25 22:36:11 +0200

    Add "RequestMethod" functions

--- src/Models/RequestMethod.php
+++ src/Models/RequestMethod.php
@@ -7,4 +7,28 @@
 		public const PUT = 'PUT';
 		public const DELETE	= 'DELETE';
 		public const PATCH = 'PATCH';
+
+		public static function get(): string {
+			return $_SERVER['REQUEST_METHOD'];
+		}
+
+		public static function isGet(): bool {
+			return self::get() === self::GET;
+		}
+
+		public static function isPost(): bool {
+			return self::get() === self::POST;
+		}
+
+		public static function isPut(): bool {
+			return self::get() === self::PUT;
+		}
+
+		public static function isPatch(): bool {
+			return self::get() === self::PATCH;
+		}
+
+		public static function isDelete(): bool {
+			return self::get() === self::DELETE;
+		}
 	}
