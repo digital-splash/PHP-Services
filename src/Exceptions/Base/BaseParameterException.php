67d45b15787edadaa27dc7aadfb4cbcd06c2c4fc
Mario Rawady <rawady_mario@hotmail.com>
2023-11-10 15:44:06 +0100

    Initial Commit DG-00037
    Fix Exceptions by Adding Code and Subcode

--- src/Exceptions/Base/BaseParameterException.php
+++ src/Exceptions/Base/BaseParameterException.php
@@ -7,11 +7,14 @@
 	class BaseParameterException extends BaseException {
 
 		public function __construct(
-			string $param
+			string $param,
+			int $code = 0,
+			int $subcode = 0,
+			int $responseCode = HttpCode::NOTFOUND
 		) {
 			$this->message = Translate::TranslateString($this->message, null, [
 				"::params::" => $param
 			]);
-			parent::__construct('', [], HttpCode::UNPROCESSABLE);
+			parent::__construct('', [], $code, $subcode, $responseCode);
 		}
 	}
