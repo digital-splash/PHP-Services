3d8368bf10d1c3d5c74c7f92fae4ffada6e0f00d
Mario Rawady <rawady_mario@hotmail.com>
2023-11-26 01:27:58 +0200

    Add Exception

---
+++ src/Exceptions/RecordAlreadyExistException.php
@@ -0,0 +1,8 @@
+<?php
+	namespace DigitalSplash\Exceptions;
+
+	use DigitalSplash\Exceptions\Base\BaseException;
+
+	final class RecordAlreadyExistException extends BaseException {
+		protected $message = "exception.AlreadyExist";
+	}
