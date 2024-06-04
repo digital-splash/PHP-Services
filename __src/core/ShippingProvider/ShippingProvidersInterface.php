e518f075efe5c8e8182a30e6f42aa5b4b7148380
Mario Rawady <rawady_mario@hotmail.com>
2024-06-04 11:36:31 +0200

    delete __src folder

--- __src/core/ShippingProvider/ShippingProvidersInterface.php
+++
@@ -1,9 +0,0 @@
-<?php
-	namespace DigitalSplash\Classes\Core\ShippingProvider;
-
-	interface ShippingProviderInterface {
-		const MINIMUM_SHIPPING_AMOUNT = 75;
-
-		public function RequestQuote(): array;
-
-	}
--- __src/database/Store.php
+++
@@ -1,6 +0,0 @@
-<?php
-	namespace DigitalSplash\Classes\Database;
-
-	class Store {
-
-    }
