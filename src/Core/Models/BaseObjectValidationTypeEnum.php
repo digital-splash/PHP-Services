17f04bd1b3340091a2696821458a501082b211ba
Mario Rawady <rawady_mario@hotmail.com>
2024-07-24 11:04:05 +0300

    Add comments

--- src/Core/Models/BaseObjectValidationTypeEnum.php
+++ src/Core/Models/BaseObjectValidationTypeEnum.php
@@ -2,7 +2,14 @@
 	namespace DigitalSplash\Core\Models;
 
 	class BaseObjectValidationTypeEnum {
+		/**
+		 * Validate only if it's missing. Exclude NULL
+		 */
 		const MISSING = 'Missing';
+
+		/**
+		 * Validate is it's missing, and if it is NULL
+		 */
 		const NOT_EMPTY = 'NotEmpty';
 
 		const ALLOWED = [
